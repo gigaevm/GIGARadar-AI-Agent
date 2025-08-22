b6ef040c9ea567bd1a265962a32dbfc89eddf73e
EVM CREW <neoalerta@gmail.com>
2025-08-22 10:04:18 +0700

    Create memeTokens.ts

---
+++ src/common/memeTokens.ts
@@ -0,0 +1,9 @@
+export const memeTokens = [
+  { name: "Seiyan", symbol: "SEIYAN", address: "0x5f0E07dFeE5832Faa00c63F2D33A0D79150E8598", verified: true, pairAddress: "0x8Ba502d9243fa5E44678044098Be9782cEbaFdA5" },
+  { name: "Popo", symbol: "POPO", address: "0xC18b6a15FB0ceaf5eb18696EeFCb5bc7b9107149", verified: true, pairAddress: "0xA2AeE4f82023c6614f3ab40041c3B434FF97B439" },
+  { name: "Milli", symbol: "MILLI", address: "0x95597EB8D227a7c4B4f5E807a815C5178eE6dBE1", verified: true, pairAddress: "0xF1d35dF54a1247b047A52F8acc2359b415Ba8A9a" },
+
+  //add more CA and Pair
+
+  ];
+console.log("[DEBUG] Loaded memeTokens with pairAddress ✅", memeTokens[0]);
